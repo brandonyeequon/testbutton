11e15f3651eab01078aeaaa80fc5d0eb876794b3
Brandon Lee <brandonyeequon@gmail.com>
2025-02-14 00:27:28 -0700

    update

--- BigButton.py
+++ BigButton.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import os
 from openai import OpenAI
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Initialize OpenAI client
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
